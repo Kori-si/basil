df7898ed666c84ac28850fcaea95aebc1d03067b
kori-si <kira-agent@mail.ru>
2023-08-11 16:24:14 +0200

    создание проекта

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Header } from "./componets/Header";
+import { Categories } from "./componets/Categories";
+import { Sort } from "./componets/Sort";
+import { ProductCard } from "./componets/ProductCard";
+
+import "./scss/app.scss";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <ProductCard title="мексиканская" price={500} />
+            <ProductCard />
+
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
